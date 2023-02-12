3e5baec4bd7f5d8ddd6373d673b12a9843c81f98
Shree Thakre <shreet365@gmail.com>
2023-02-12 10:16:19 +0530

    number of times the dice was played And Position of Dice

--- CheckPosition.cs
+++ CheckPosition.cs
@@ -10,44 +10,62 @@ namespace SnakeAndLadderProblem
     {
         public void CheckPositioNofPlayer()
         {
+            int iPosition = 0, iCount = 0;
+
             Console.WriteLine("Enter Player name");
             string iPlayer1 = Convert.ToString(Console.ReadLine());
 
-            int iPosition = 0;
             while (iPosition <= 100)
             {
                 Random random = new Random();
-                Console.ReadLine();
-
                 int iDice = random.Next(1, 7);
+
+                iCount++;
+                Console.WriteLine("Dice Rolled {0}", iCount);
+
                 iPosition += iDice;
-                Console.WriteLine("Dice Rolled :{0} \nPosition is {1}", +iDice, iPosition);
+                Console.WriteLine("Dice Number is :{0} \nPosition is {1}\n", +iDice, iPosition);
 
+                if (iPosition > 100)
+                {
+                    iPosition = iPosition - iDice;
+                    Console.WriteLine("Not Valid Please Tray Again {0}", iPosition);
 
-                if ((iPosition == 99) || (iPosition == 95) || (iPosition == 87) || (iPosition == 65)) // Snake
+                }
+                else if ((iPosition == 99) || (iPosition == 95) || (iPosition == 87) || (iPosition == 65)) // Snake
                 {
                     iPosition -= 17;
-                    Console.WriteLine("You Got Sneke new Position is {0}", iPosition);
+                    Console.WriteLine("You Got Sneke Position is {0}", iPosition);
                 }
                 else if ((iPosition == 62) || (iPosition == 54) || (iPosition == 17))   // Snake
                 {
                     iPosition -= 9;
-                    Console.WriteLine("You Got Sneke new Position is {0}", iPosition);
+                    Console.WriteLine("You Got Sneke Position is {0}", iPosition);
                 }
                 else if ((iPosition == 4) || (iPosition == 9) || (iPosition == 51) || (iPosition == 40) || (iPosition == 63) || (iPosition == 51))// Ladder
                 {
-                    iPosition += 13;        
-                    Console.WriteLine("You Got Ladder new Position is {0}", iPosition);
+                    iPosition += 13;
+                    Console.WriteLine("You Got Ladder Position is {0}", iPosition);
                 }
                 else if (iPosition == 24) // Ladder
                 {
                     iPosition += 48;
-                    Console.WriteLine("You Got Ladder new Position is {0}", iPosition);
+                    Console.WriteLine("You Got Ladder Position is {0}", iPosition);
                 }
+                else if (iPosition <= 0)
+                {
+                    iPosition = 0;   // reset
 
+                }
+                else if (iPosition == 100)
+                {
+                    break;
+
+                }
             }
-
+            Console.WriteLine("Winner is : {0}\nTotal Number of times Dice Rolled : {1}", iPlayer1, iCount);
         }
     }
-}
+ }
+
 
