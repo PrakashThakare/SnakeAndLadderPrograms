58fb881af94d7edac171712632fccb333b169b16
Shree Thakre <shreet365@gmail.com>
2023-02-12 08:09:48 +0530

    Adding Welcome Massage

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SnakeAndLadderProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("************** Welcome To Snake And Ladder Programs ******************");
+            Console.ReadLine();
+        }
+    }
+}
